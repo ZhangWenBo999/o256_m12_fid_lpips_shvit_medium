f4e9ff5f3fc3ca1f62adcffd2cf230aca94c6f2c
ZhangWenBo999 <131420945+ZhangWenBo999@users.noreply.github.com>
2025-01-10 10:42:40 +0800

    Update unet.py

--- models/guided_diffusion_modules/unet.py
+++ models/guided_diffusion_modules/unet.py
@@ -459,14 +459,14 @@ class UNet(nn.Module):
                 use_checkpoint=use_checkpoint,
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
-            AttentionBlock(
-                ch,
-                use_checkpoint=use_checkpoint,
-                num_heads=num_heads,
-                num_head_channels=num_head_channels,
-                use_new_attention_order=use_new_attention_order,
-            ),
-            # SHViTBlock(ch, type='s'),
+            # AttentionBlock(
+            #     ch,
+            #     use_checkpoint=use_checkpoint,
+            #     num_heads=num_heads,
+            #     num_head_channels=num_head_channels,
+            #     use_new_attention_order=use_new_attention_order,
+            # ),
+            SHViTBlock(ch, type='s'),
             ResBlock(
                 ch,
                 cond_embed_dim,
